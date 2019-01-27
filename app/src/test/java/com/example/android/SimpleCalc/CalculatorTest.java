adad7253e8e3c5923661ca38d94726ec2886743b
Hugo Guillín <hugoguillin@hotmail.com>
2019-01-27 16:59:28 +0100

    Task_2

--- app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
+++ app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
@@ -25,6 +25,7 @@ import org.junit.runners.JUnit4;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -53,6 +54,51 @@ public class CalculatorTest {
         assertThat(resultAdd, is(equalTo(2d)));
     }
 
+    @Test
+    public void addTwoNumbersNegative() {
+        double resultAdd = mCalculator.add(-1d, 2d);
+        assertThat(resultAdd, is(equalTo(1d)));
+    }
 
+    @Test
+    public void addTwoNumbersFloats() {
+        double resultAdd = mCalculator.add(1.111f, 1.111d);
+        assertThat(resultAdd, is(closeTo(2.222, 0.01)));
+    }
 
+    @Test
+    public void restarDosNumeros() {
+        double resultado = mCalculator.sub(8d, 5d);
+        assertThat(resultado, is(equalTo(3d)));
+    }
+
+    @Test
+    public void restarNegativos() {
+        double resultado = mCalculator.sub(-2d, -5d);
+        assertThat(resultado, is(equalTo(3d)));
+    }
+
+    @Test
+    public void multiplicaDosNumeros() {
+        double resultado = mCalculator.mul(5d, 4d);
+        assertThat(resultado, is(equalTo(20d)));
+    }
+
+    @Test
+    public void multiplicaNegativos() {
+        double resultado = mCalculator.mul(532d, -325d);
+        assertThat(resultado, is(equalTo(-172900d)));
+    }
+
+    @Test
+    public void divideDosNumeros() {
+        double resultado = mCalculator.div(25d, 5d);
+        assertThat(resultado, is(equalTo(5d)));
+    }
+
+    @Test
+    public void divideEntreCero() {
+        double resultado = mCalculator.div(9d, 0d);
+        assertThat(resultado, is(equalTo(Double.POSITIVE_INFINITY)));
+    }
 }
